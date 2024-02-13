1ad11f45e6b0a2f9b1a44ca5bb0f82f308fdbdde
HJska27 <kallvey.thong23@student.cart.org>
2024-02-12 18:20:57 -0800

    wer

--- index.js
+++ index.js
@@ -1,38 +1,92 @@
-
-
-
 // Add Event listners when DOM is ready
 document.addEventListener("DOMContentLoaded", function () {
 
-    // check for buttons exist
-    if ( document.querySelectorAll('.button') !== null) {
-        let buttons = document.querySelectorAll('.button');
-        buttons.forEach(button => button.addEventListener('click', processForm))
-    }
+  // check for buttons exist
+  if ( document.querySelectorAll('.button') !== null) {
+      let buttons = document.querySelectorAll('.button');
+      buttons.forEach(button => button.addEventListener('click', processForm))
+  }
 
-    // optionally update when fields update
-    // tbd
+  // optionally update when fields update
+  // tbd
+  document.getElementById('name').addEventListener('change', function (eventData) {
+      let nameOutput = document.getElementById('nameOutput');
+      // console.log(eventData);
+      nameOutput.innerText = eventData.target.value;
+  });
 
-    // process form Data
-    document.getElementById("myForm").addEventListener("submit", function (e) {
-        e.preventDefault();
-      
-        var formData = new FormData(myForm);
-        // output as an object
-        console.log(Object.fromEntries(formData));
-      
-        // ...or iterate through the name-value pairs
-        for (var pair of formData.entries()) {
-          console.log(pair[0] + ": " + pair[1]);
-        }
-      });
-    // Log readiness to console
-    console.log("Ready");
+  document.getElementById('type').addEventListener('change', function (eventData) {
+      let nameOutput = document.getElementById('nameOutput');
+      // console.log(eventData);
+      let info = document.getElementById('info');
+      info.innerText = eventData.target.value;
+      changeCert(eventData.target.value);
+  });
+
+  // process form Data
+  document.getElementById("myForm").addEventListener("submit", function (eventData) {
+      eventData.preventDefault(); //stop page reload when form is submitted
+      console.log(eventData.target);
+      var formData = new FormData(eventData.target);
+      formData = Object.fromEntries(formData);
+
+      //update certificate information when form is submitted:
+      let nameOutput = document.getElementById('nameOutput');
+      let info = document.getElementById('info');
+      nameOutput.innerText = formData.name;
+      info.innerText = formData.type;
+
+    });
+  // Log readiness to console
+  console.log("Ready");
+
+  // let info = document.getElementById('info');
+  // info.classList.add('test');
+  
+  
+  
+  // certificate.classList.add('number1');
+  
+  function changeCert(value){
+      let certificate = document.getElementById('certificate');
+      certificate.classList = ""; // clear classes on each function call
+      switch(value){
+          case 'A':
+              certificate.classList.add('number1');
+          break;
+          case 'B':
+              certificate.classList.add('number2');
+          break;
+          case 'C':
+              certificate.classList.add('number3');
+          break;
+      }
+  }
+
+  // Adding a button to print the certificate
+  // setup print event listener
+  if ( document.getElementById('print') !== null) {
+      let printElement = document.getElementById('print');
+      printElement.addEventListener('click', function(e){
+      window.print();
+      console.log("invoke print");
+      } );
+  } // or in html -> onclick="window.print()"
+
+
+  let theDate = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"numeric", day:"numeric"}) 
+
+  let dateBox = document.getElementById('theDate');
+  dateBox.innerText = theDate;
 });
+
+
+
+
+
+
+
 
 function processForm(form){
 
 }
-
-
-let theDate = new Date().toLocaleDateString('en-us')
